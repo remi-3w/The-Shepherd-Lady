12f485b48ecaa10cf77da9dff769917b09b4a242
remi <remi_94@msn.com>
2021-08-19 15:38:59 +0200

    creations des variables et twig de product

--- TSL-APP/src/Controller/ShopController.php
+++ TSL-APP/src/Controller/ShopController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ShopController extends AbstractController
 {
     /**
      * @Route("/boutique", name="shop")
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products= $productRepository->findAll();
+        //dd($products);
+     
+
         return $this->render('shop/index.html.twig', [
-            'controller_name' => 'ShopController',
+                        'products' => $products
         ]);
     }
 }
