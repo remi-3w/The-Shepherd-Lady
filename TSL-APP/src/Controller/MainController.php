432cb4fde273f2a1ce389da7b5c82ec4d59b127c
remi <remi_94@msn.com>
2021-08-27 13:54:12 +0200

    configuration du role admin + mise en palce de l'authentification + bouton login/logout du menu

--- TSL-APP/src/Controller/MainController.php
+++ TSL-APP/src/Controller/MainController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\LoginType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class MainController extends AbstractController
 {
@@ -17,4 +20,26 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils) : Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $form = $this->createForm(LoginType::class);
+        return $this->render('main/login.html.twig',[
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'formView' =>$form->createView(), 
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+   public function logout(){
+
+   }
 }
